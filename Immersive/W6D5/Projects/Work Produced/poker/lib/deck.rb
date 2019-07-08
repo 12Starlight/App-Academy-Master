acf557ceea5bd6e443bc84cd0e4d7be4b5e1309b
Dave <dave.gagnat@gmail.com>
2019-07-08 16:56:11 -0500

    Learned how to build deck class from scratch, @poker

--- Immersive/W6D5/Projects/Work Produced/poker/lib/deck.rb
+++ Immersive/W6D5/Projects/Work Produced/poker/lib/deck.rb
@@ -4,30 +4,32 @@ require_relative './hand'
 class Deck
   #What is this? What does it do?
   def self.all_cards
-    
+    combined = Card.suits.product(Card.values)
+    combined.map { |suit, value| Card.new(suit, value) }
   end
 
   def initialize(cards = Deck.all_cards)
-    
+    @cards = cards 
   end
 
   def deal_hand
-    
+    Hand.new(self.take(5))
   end
 
   def count
-    
+    @cards.count 
   end
 
   def take(n)
-    
+    raise "not enough cards" if count < n 
+    @cards.shift(n)
   end
 
   def return(cards)
-    
+    @cards += cards 
   end
 
   def shuffle
-    
+    @cards.shuffle!
   end
 end

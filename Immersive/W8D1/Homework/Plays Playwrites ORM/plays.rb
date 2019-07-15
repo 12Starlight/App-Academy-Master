9814594dd85a1ffb5ee9573a344487f8d4a0e971
Dave <dave.gagnat@gmail.com>
2019-07-15 08:55:30 -0500

    built update

--- Immersive/W8D1/Homework/Plays Playwrites ORM/plays.rb
+++ Immersive/W8D1/Homework/Plays Playwrites ORM/plays.rb
@@ -122,5 +122,15 @@ class Playwrite
     self.id = PlayDBConnection.instance.last_insert_row_id
   end
 
-  
+  def update
+    raise "#{self} not in database" unless self.id 
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ? 
+      WHERE
+        id = ?
+    SQL
+  end
 end

c777a53f7708efd7ac786cc3cf45d1c42b93b53a
Dave <dave.gagnat@gmail.com>
2019-07-20 03:45:15 -0500

    Drilled 05_aggregates, 3 times without solutions

--- Immersive/W7D4/Projects/SQLzoo/lib/05_aggregates.rb
+++ Immersive/W7D4/Projects/SQLzoo/lib/05_aggregates.rb
@@ -24,58 +24,88 @@ end
 
 def continents
   # List all the continents - just once each.
-  # DISTINCT(continent) - removes the duplicate continents
   execute(<<-SQL)
-
+    SELECT
+      DISTINCT
+        continent
+    FROM 
+      countries 
   SQL
 end
 
 def africa_gdp
   # Give the total GDP of Africa.
-  # Do not need to GROUP BY, just returning SUM
   execute(<<-SQL)
-  
+    SELECT
+      SUM(gdp)
+    FROM
+      countries
+    WHERE
+      continent = 'Africa'
   SQL
 end
 
 def area_count
   # How many countries have an area of more than 1,000,000?
-  # Just returning COUNT
   execute(<<-SQL)
-  
+    SELECT
+      COUNT(name)
+    FROM
+      countries
+    WHERE
+      area > 1000000
   SQL
 end
 
 def group_population
   # What is the total population of ('France','Germany','Spain')?
-  # We are returning SUM, but comparing names IN
   execute(<<-SQL)
-  
+    SELECT
+      SUM(population)
+    FROM
+      countries
+    WHERE
+      name IN ('France', 'Germany', 'Spain')
   SQL
 end
 
 def country_counts
   # For each continent show the continent and number of countries.
-  # COUNT(name) or COUNT(*) will count all the names
   execute(<<-SQL)
-
+    SELECT
+      continent, COUNT(name)
+    FROM
+      countries
+    GROUP BY
+      continent 
   SQL
 end
 
 def populous_country_counts
   # For each continent show the continent and number of countries with
   # populations of at least 10 million.
-  # GROUP BY goes after WHERE
-  # FROM, WHERE, GROUP BY, HAVING, SELECT, ORDER, LIMIT
-
   execute(<<-SQL)
-    
+    SELECT
+      continent, COUNT(name)
+    FROM
+      countries
+    WHERE
+      population >= 10000000
+    GROUP BY
+      continent 
   SQL
 end
 
 def populous_continents
   # List the continents that have a total population of at least 100 million.
   execute(<<-SQL)
- 
+    SELECT
+      continent
+    FROM
+      countries
+    GROUP BY
+      continent
+    HAVING
+      SUM(population) >= 100000000
   SQL
 end

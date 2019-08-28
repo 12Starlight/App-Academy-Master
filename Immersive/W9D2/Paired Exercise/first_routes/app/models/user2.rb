7b7ddb4ab27707380a46fa77bf4e37a09db06fee
Dave <dave.gagnat@gmail.com>
2019-08-28 01:34:29 -0500

    Paired Exercise II, partner @Noah

--- Immersive/W9D2/Paired Exercise/first_routes/app/models/user2.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-
-    # create_table "users", force: cascade do |t|
-    #     t.string "name", null: false
-    #     t.string "email", null: false
-    #     t.timestamps
-    # end
-
-end

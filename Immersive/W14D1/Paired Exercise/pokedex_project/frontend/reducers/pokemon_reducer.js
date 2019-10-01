8c870993a0f37e15eec2d80cdf69d48cb19f413c
Dave <dave.gagnat@gmail.com>
2019-09-30 19:05:06 -0500

    Paired Exercise, partner @Raymond

---
+++ Immersive/W14D1/Paired Exercise/pokedex_project/frontend/reducers/pokemon_reducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_ALL_POKEMON } from '../actions/pokemon_actions';
+
+
+const pokemonReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch(action.type) {
+    case RECEIVE_ALL_POKEMON:
+      let newState = {};
+      action.pokemon.forEach(pokemon => {
+        newState[pokemon.id] = pokemon;
+      }); 
+      return newState; 
+    default:
+      return state; 
+  }
+};
+
+export default pokemonReducer;

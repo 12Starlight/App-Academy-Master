0f3363aca72b930cb1d62a4f1fae70fa4899bb63
Dave <dave.gagnat@gmail.com>
2019-10-01 04:04:47 -0500

    Added W13D3 Readings, HW Job Listing, Todos

---
+++ Immersive/React Nightly Review/10 W13D3 Readings Master/fruit_stand_02/frontend/entry.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+
+import store from "./store";
+import { addOrange, addApple, clearFruit } from "./actions";
+
+// Todo just for testing!
+window.store = store;
+window.addOrange = addOrange;
+window.addApple = addApple;
+window.clearFruit = clearFruit;
+
+import FruitStandContainer from "./components/fruit_stand_container";
+
+const App = () => (
+	<Provider store={ store }>
+		<FruitStandContainer /> 
+	</Provider>
+);
+
+document.addEventListener("DOMContentLoaded", () => {
+	ReactDOM.render(
+		<App />,
+		document.getElementById('root')
+	);  
+}); 
+  

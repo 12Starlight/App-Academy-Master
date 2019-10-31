b55efa89d5e68a85126305aa5cbf30b3bc4ddd90
Dave <dave.gagnat@gmail.com>
2019-10-31 06:21:38 -0500

    Removed A04

--- Immersive/W11D1/Dave_Gagnat_A04/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-  resource :session, only: [:new, :create, :destroy]
-  resources :lists do
-    resources :tasks, only: [:create]
-  end
-
-  resources :tasks, only: [:destroy]
-end

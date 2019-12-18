024b234dd465e93f50a10b0e4d341c7552017806
Dave <dave.gagnat@gmail.com>
2019-12-18 11:13:12 -0600

    Removed A05 practice generator

--- Immersive/W12D1/A05-practice-generator-master/problems/deep_dup.js
+++
@@ -1,8 +0,0 @@
-// Write a function, `deepDup(arr)`, that will perform a "deep" duplication of the
-// array and any interior arrays. A deep duplication means that the array itself,
-// as well as any nested arrays (no matter how deeply nested) are duped and are
-// completely different objects in memory than those in the original array.
-function deepDup(arr) {
-  return arr.map(e => e instanceof Array ? deepDup(e) : e)
-};
-console.log(deepDup([{}, [{ color: "yellow" }, 2, "coders rule!"]]));
